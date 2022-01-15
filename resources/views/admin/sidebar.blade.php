bff6aad60336c232413cb73adb36639ea2ee65c0
bagashezky <bagashpratama95@gmail.com>
2022-01-15 18:49:43 +0700

    Koneksi login register ke templating

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -15,7 +15,7 @@
           <img src="{{ asset('lte/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">Alexander Piercee</a>
         </div>
       </div>
 
@@ -39,4 +39,5 @@
       <!-- /.sidebar-menu -->
     </div>
     <!-- /.sidebar -->
+
   </aside>
