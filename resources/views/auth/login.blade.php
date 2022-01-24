121da552d7a4a37e48a348147cb400dd14179c59
bagashezky <bagashpratama95@gmail.com>
2022-01-24 20:28:13 +0700

    Dashboard Card Info

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+
+<style>
+    body {
+     background-color: rgb(0, 146, 194);
+    }
+  </style>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -10,7 +16,6 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
